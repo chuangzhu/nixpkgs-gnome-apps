98877758ee4cc1970f726cf887dbb284be73254b
Chuang Zhu <git@chuang.cz>
2024-11-06 17:20:13 +0800

    - pipeline gtuber

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,11 @@
 { callPackage, ... }:
 
-rec {
+{
   purism-stream = callPackage ./purism-stream.nix { };
   gadgetcontroller = callPackage ./gadgetcontroller.nix { };
   phosh-osk-stub= callPackage ./phosh-osk-stub.nix { };
   bunker = callPackage ./bunker.nix { };
   avvie = callPackage ./avvie.nix { };
   flashcards = callPackage ./flashcards.nix { };
-  gtuber = callPackage ./gtuber.nix { };
-  pipeline = callPackage ./tubefeeder.nix { inherit gtuber; };
   dewduct = callPackage ./dewduct.nix { };
 }
